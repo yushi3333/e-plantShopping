c0226733706e6fbf724692b49f6c4ecd691bfe6a
yushi3333 <a1125189291@gmail.com>
2024-09-16 11:49:48 -0400

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -254,6 +254,10 @@ function ProductList() {
            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
          }));
     };
+    const calculateTotalItemQuantity = () => {
+        const items = cart.map((item) => item.quantity);
+        return items.reduce((total, quantity) => total + quantity, 0)
+    }
 
     const handlePlantsClick = (e) => {
         e.preventDefault();
@@ -281,13 +285,29 @@ function ProductList() {
                     </div>
                 
                 </div>
-            <div style={styleObjUl}>
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1>
-                {totalQuantity > 0 && (
-                                <span style={styleA}>{totalQuantity}</span>
-                            )}
-                </a></div>
+                <div style={styleObjUl}>
+                    <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                    <div> <a href="#" style={styleA}>Groceries</a></div>
+                    <div> <a href="#" style={styleA}>Electroncies</a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <h1 className='cart'>
+                            <svg xmlns="http://www.w3.org/2000/svg" 
+                            viewBox="0 0 256 256" 
+                            id="IconChangeColor" 
+                            height="68" width="68"
+                            >
+                            <rect width="156" height="156" fill="none"></rect>    
+                            <circle cx="80" cy="216" r="12"></circle>
+                            <circle cx="184" cy="216" r="12"></circle>
+                            <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                            </svg>
+                            <h1 className='cart_quantity_count'>
+                                {calculateTotalItemQuantity()}
+                            </h1>
+
+                            </h1>
+                    
+                    </a></div>
             </div>
         </div>
         {!showCart? (
